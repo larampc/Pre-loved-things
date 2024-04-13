a0547a38df101521046e2df9fd1cf805d88c5385
Francisco <up202208485@up.pt>
2024-04-13 19:41:49 +0100

    translated user.html to user.php

--- templates/item.tpl.php
+++ templates/item.tpl.php
@@ -1,29 +1,27 @@
-<?php declare(strict_types = 1); ?>
+<?php
 
-<?php function drawItems(array $items) { ?>
+function drawItem($item) { ?>
+    <div class="item-main">
+        <img src="<?="images/" . $item['imagePath']?>" alt="<?= explode($item['imagePath'],'.')[0]?>">
+        <div class="item-info">
+            <p class="name"><?=$item['name']?></p>
+            <p class="price"><?=$item['price']?></p>
+        </div>
+    </div>
+<?php }
+
+function drawItems(array $items) { ?>
     <h2>Last Added Items</h2>
     <section class="items">
-        <?php foreach($items as $item) { ?>
-        <div class="item-main">
-            <img src="<?="images/" . $item['imagePath']?>" alt="<?= explode($item['imagePath'],'.')[0]?>"  >
-                <div class="item-info">
-                    <p class="name"><?=$item['Name']?></p>
-                    <p class="price"><?=$item['Price']?></p>
-                </div>
-        </div>
-       <?php } ?>
+        <?php foreach($items as $item) {
+            drawItem($item);
+        } ?>
     </section>
     <h2>Most liked</h2>
     <section class="items">
-    <?php foreach($items as $item) { ?>
-        <div class="item-main">
-            <img src="<?="images/" . $item['imagePath']?>" alt="<?= explode($item['imagePath'],'.')[0]?>"  >
-            <div class="item-info">
-                <p class="name"><?=$item['Name']?></p>
-                <p class="price"><?=$item['Price']?></p>
-            </div>
-        </div>
-       <?php } ?>
+    <?php foreach($items as $item) {
+        drawItem($item);
+    } ?>
     </section> 
 <?php } ?>
 
