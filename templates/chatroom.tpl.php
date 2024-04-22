bdb2c9815019fb7a7d8e08d0b84a40727d030d2c
Francisco <up202208485@up.pt>
2024-04-22 15:53:59 +0100

    chatroom.tpl.php changes

--- templates/chatroom.tpl.php
+++ templates/chatroom.tpl.php
@@ -11,10 +11,10 @@ function draw_user_chatrooms(array $chatrooms, User $from) : void { ?>
     <?php
 }
 function draw_small_chatroom(Chatroom $chatroom, User $from, User $to) : void { ?>
-    <div class="chat">
+    <div class="chat" id=<?= "chat" . $chatroom->chatroomId ?>>
         <img src=<?= "images/" . $to->photoPath?> />
         <div class="chat-content">
-            <h4><?= $to->name?>></h4>
+            <h4><?= $to->name?></h4>
             <p> <?= $chatroom->last_message->message ?></p>
         </div>
         <p><?=$chatroom->unread_message_count?></p>
