8f60294057c7c2c0a69b8a3177876db152aa7559
Lara <up202208689@g.uporto.pt>
2024-04-22 15:17:16 +0100

    Fixed login

--- action_login.php
+++ action_login.php
@@ -8,9 +8,8 @@ require_once('database/users.db.php');
 
 $dbh = get_database_connection();
 $user = verify_user($dbh, $_POST['email'], $_POST['password']);
-if (!empty($user)) {
-  $_SESSION['user_id'] = $user['user_id'];
-  var_dump($_SESSION['cart']);
-  if ($_SESSION['cart']) add_to_cart($dbh, $_SESSION['cart'], $user['user_id']);
+if ($user != -1) {
+  $_SESSION['user_id'] = $user;
+  if ($_SESSION['cart']) add_to_cart($dbh, $_SESSION['cart'], $user);
   header('Location: main.php');
 } else  header('Location: login.php');
