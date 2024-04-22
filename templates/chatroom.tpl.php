543d217802d9aae24e12550335f2aa3bc8539ae1
Francisco <up202208485@up.pt>
2024-04-22 10:43:46 +0100

    fixed draw_small_chatroom

--- templates/chatroom.tpl.php
+++ templates/chatroom.tpl.php
@@ -12,8 +12,11 @@ function draw_user_chatrooms(array $chatrooms, User $from) : void { ?>
 }
 function draw_small_chatroom(Chatroom $chatroom, User $from, User $to) : void { ?>
     <div class="chat">
-        <h4><?= $to->name?>></h4>
         <img src=<?= "images/" . $to->photoPath?> />
-        <p> <?= $chatroom->last_message->message ?></p>
+        <div class="chat-content">
+            <h4><?= $to->name?>></h4>
+            <p> <?= $chatroom->last_message->message ?></p>
+        </div>
+        <p><?=$chatroom->unread_message_count?></p>
     </div>
 <?php }
