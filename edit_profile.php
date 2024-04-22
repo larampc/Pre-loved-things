a5bd69c3b2092a74526775eed8cf03571be4183e
Bruno <up202205619@up.pt>
2024-04-22 15:46:50 +0100

    Improved css and inbox2.php

--- edit_profile.php
+++ edit_profile.php
@@ -7,6 +7,6 @@ require_once('database/users.db.php');
 require_once('templates/user.tpl.php');
 
 $db = get_database_connection();
-draw_header("profile");
+draw_header("editprofile");
 draw_edit_profile(get_user($db, (int)$_SESSION['user_id']));
 draw_footer();
