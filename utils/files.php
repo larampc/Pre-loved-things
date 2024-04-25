2e75d08a1c6f0d6985051a33e199e676159f3498
Francisco <up202208485@up.pt>
2024-04-25 23:19:35 +0100

    bug in files.php

--- utils/files.php
+++ utils/files.php
@@ -2,7 +2,7 @@
 function upload_image($img)
 {
 
-    $target_dir = "images/";
+    $target_dir = "../images/";
     $target_file = $target_dir . basename($_FILES[$img]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
