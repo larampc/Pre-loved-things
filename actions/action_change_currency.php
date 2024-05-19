95edb46bcc4248cb4904a56384d4bcbcfd01b7f9
Francisco <up202208485@up.pt>
2024-05-19 20:28:12 +0100

    small things

--- actions/action_change_currency.php
+++ actions/action_change_currency.php
@@ -3,11 +3,7 @@
 declare(strict_types=1);
 
 require_once(__DIR__ . '/../utils/session.php');
-require_once(__DIR__ . '/../utils/logger.php');
 $session = new Session();
-
-log_to_stdout($_GET['csrf']);
-log_to_stdout($_SESSION['csrf']);
 
 if (!validate_csrf_token($_GET['csrf'])) {
     die(header('Location: ' . $_SERVER['HTTP_REFERER']));
